9a9b713dfc1bd70e31577d396122eff37510b92e
Alex Rodrigues <alexmusicalsa@gmail.com>
2022-12-03 21:42:30 -0300

    indexedDB

--- assets/app.js
+++ assets/app.js
@@ -117,3 +117,52 @@ function exportReportToExcel() {
         name: "planilha.xlsx", sheet: { name: "planilha" }
     });
 }
+
+/*
+    O que e indexedDB
+  - banco de dados nao relacional
+  - permite armazenamento de objetos no browser do usuário
+  - nos permite armazenar objetos javascript, arquivos, blobs
+  - suporta transacoes
+  - la podemos definir multiplos indices para realizar consultas
+  Bibliotecas que facilitam o uso de indexedDB
+  - IndexedDB Promised
+  - localForage
+  // - https://www.youtube.com/watch?v=vb7fkBeblcw
+*/
+
+let outDB;
+let db;
+
+// function to create database
+const createDB = () => {
+    if (window.indexedDB) {
+        const request = window.indexedDB.open("MyWeatherDB", 1);
+
+        request.onerror = (event) => {
+            console.log('Error request', event);
+            outDB.innerHTML = 'Error request';
+        }
+
+        request.onsuccess = (event) => {
+            db = request.result;
+            console.log('Successed request', event, db);
+            outDB.innerHTML = 'Successed request';
+        }
+
+        request.onupgradeneeded = () => {
+            console.log('Upgraded request', event)
+            outDB.innerHTML = 'Upgraded request';
+        }
+
+    } else {
+        console.log('You don\'t have support');
+        outDB.innerHTML = 'Upgraded request';
+    }
+}
+
+// execute script when the DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    outDB = document.getElementById('output-db');
+    createDB();
+});
